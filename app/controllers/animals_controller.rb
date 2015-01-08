33571e537c5c6cc224c104500666d7b0a87b6419
Ian <iantskon@gmail.com>
2015-01-07 19:27:23 -0800

    Add CR of CRUD

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -7,9 +7,14 @@ class AnimalsController < ApplicationController
   end
 
   def create
+  	@animal = Animal.new(animal_params)
+  	@animal.save
+
+  	redirect_to @animal
   end
 
   def show
+  	@animal = Animal.find(params[:id])
   end
 
   def edit
@@ -19,6 +24,11 @@ class AnimalsController < ApplicationController
   end
 
   def destroy
+	end
+
+private
+	def animal_params
+		params.require(:animal).permit(:name, :species, :img)
 	end
 
 end
