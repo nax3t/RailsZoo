731a1a00f2a90795c59a21ff9c0c073c0dcaa04d
Ian <iantskon@gmail.com>
2015-01-07 18:41:53 -0800

    Init git

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,24 @@
+class AnimalsController < ApplicationController
+  def index
+  	@animals = Animal.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+	end
+
+end
